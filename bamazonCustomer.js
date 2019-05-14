f73fe94a09b2ab1150ca9255aca233e5c05989a8
Kat Vengo <katvengo@gmail.com>
2019-05-14 10:01:55 -0700

    working inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,12 +30,45 @@ function displayStartingValues() {
             if (err) throw err;
             console.log("Products for sale!")
             for (let i = 0; i < res.length; i++) {
-            console.log("Item id Number:" + " " + res[i].item_id + "   " + "Name:" + " " + res[i].product_name + "   "
-+ "Price:" + " "  + "$" + res[i].price       );
-                
+                console.log("Item id Number:" + " " + res[i].item_id + "   " + "Name:" + " " + res[i].product_name + "   " +
+                    "Price:" + " " + "$" + res[i].price);
+
             }
 
         }
 
     )
+    run()
+}
+
+function run() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([{
+                name: "Items for Sale",
+                type: "rawlist",
+                mesage: "What would you like to buy?",
+                choices: function () {
+                    var choiceArray = []
+                    for (let i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].product_name);
+                    }
+                    return choiceArray
+
+                }
+
+            }])
+            .then(function (answer) {
+                var chosenItem;
+
+                for (let i = 0; i < results.length; i++) {
+                    if (results[i].product_name === answer.choice) {
+                        chosenItem = results[i]
+                    }
+
+                }
+            })
+    })
 }
